f94672a6458e35f938ab7194e58e018cfd239179
Matt Shwery <matthew.shwery@segment.com>
2020-09-11 18:03:28 -0500

    v6 Badge, Pill, Avatar theming (#1003)
    * move Badge and Pill to new theme
    
    * move Avatar to theme
    
    * update migration doc

--- index.d.ts
+++ index.d.ts
@@ -515,7 +515,6 @@ export interface BadgeOwnProps extends StrongOwnProps {
    * Whether or not to apply hover/focus/active styles.
    */
   isInteractive?: boolean
-  isSolid?: boolean
 }
 
 export type BadgeProps = PolymorphicBoxProps<'strong', BadgeOwnProps>
