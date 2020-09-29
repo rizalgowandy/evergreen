e11b8f66e879c087d16e8cf0a9445d3bb2fd8e36
colinlohner <colin@colinlohner.com>
2020-09-29 12:16:39 -0700

    Decouple Autocomplete item from Select Menu Option

--- src/autocomplete/src/AutocompleteItem.js
+++ src/autocomplete/src/AutocompleteItem.js
@@ -1,29 +1,90 @@
 import React, { memo, forwardRef } from 'react'
+import cx from 'classnames'
 import PropTypes from 'prop-types'
-import Option from '../../select-menu/src/Option'
+import { useStyleConfig } from '../../hooks'
+import { Image } from '../../image'
+import { Pane } from '../../layers'
+import { pseudoSelectors } from '../../select-menu/src/Option'
+import TableRow from '../../table/src/TableRow'
+import TextTableCell from '../../table/src/TextTableCell'
 
-const AutocompleteItem = memo(
-  forwardRef(function AutocompleteItem(props, ref) {
-    const { children, isHighlighted, isSelected, style, ...restProps } = props
+const internalStyles = {
+  alignItems: 'center',
+  display: 'flex'
+}
+
+const emptyObject = {}
+
+const Option = memo(
+  forwardRef(function Option(props, ref) {
+    const {
+      children,
+      className,
+      disabled,
+      height,
+      icon,
+      isHighlighted,
+      isSelectable,
+      isSelected,
+      onDeselect,
+      onSelect,
+      style,
+      ...rest
+    } = props
+
+    const { className: themedClassName, ...boxProps } = useStyleConfig(
+      'Option',
+      emptyObject,
+      pseudoSelectors,
+      internalStyles
+    )
 
     return (
-      <Option
-        ref={ref}
+      <TableRow
+        className={cx(themedClassName, className)}
+        isSelectable={isSelectable && !disabled}
         isHighlighted={isHighlighted}
+        onSelect={onSelect}
+        onDeselect={onDeselect}
         isSelected={isSelected}
-        label={children}
         style={style}
-        {...restProps}
-      />
+        {...boxProps}
+        {...rest}
+        ref={ref}
+      >
+        <TextTableCell
+          borderRight={null}
+          flex={1}
+          alignSelf="stretch"
+          height={height}
+          cursor={disabled ? 'default' : 'pointer'}
+        >
+          <Pane alignItems="center" display="flex">
+            {icon && <Image src={icon} width={24} marginRight={8} />}
+            {children}
+          </Pane>
+        </TextTableCell>
+      </TableRow>
     )
   })
 )
 
-AutocompleteItem.propTypes = {
-  children: PropTypes.node,
-  style: PropTypes.object,
+Option.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+    PropTypes.string
+  ]),
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  style: PropTypes.any,
+  height: PropTypes.number,
+  onSelect: PropTypes.func,
+  onDeselect: PropTypes.func,
+  isHighlighted: PropTypes.bool,
   isSelected: PropTypes.bool,
-  isHighlighted: PropTypes.bool
+  isSelectable: PropTypes.bool,
+  disabled: PropTypes.bool
 }
 
-export default AutocompleteItem
+export default Option
